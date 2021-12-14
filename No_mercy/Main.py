95f407935bde832b4a0529c8b1c84bcdb8ec587e
GabrielDuke <42377719+Gabriel018@users.noreply.github.com>
2021-12-14 11:36:00 -0300

    Add files via upload

---
+++ No_mercy/Main.py
@@ -0,0 +1,174 @@
+import sys
+import pygame
+import self
+from pygame import *
+
+height = 700
+width = 500
+pos_x = 100
+pos_y = 380
+
+tela = pygame.display.set_mode((height,width))
+
+
+class Jogo(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        music_fundo = pygame.mixer.music.load('music/music-battle.mp3')
+        pygame.mixer.music.set_volume(0.4)
+        pygame.mixer.music.play(-1)
+
+        self.pos_x = 100
+        self.pos_y = 380
+
+        self.Sprites = []
+        self.Sprites.append(pygame.image.load('img/john/jonh1.png'))
+        self.Sprites.append(pygame.image.load('img/john/jonh2.png'))
+        self.Sprites.append(pygame.image.load('img/john/jonh3.png'))
+        self.Sprites.append(pygame.image.load('img/john/jonh4.png'))
+        self.image_atual = 0
+        self.image = self.Sprites[self.image_atual]
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.pos_x,self.pos_y
+        self.image = pygame.transform.scale(self.image, (80, 80))
+        self.mover = False
+
+
+
+
+
+
+    def mover_frente(self):
+       self.mover = True
+       self.rect.x += 10
+       if  self.rect.x >= 520:
+           self.rect.x = 520
+       if not  key[pygame.K_RIGHT]:
+          self.mover = False
+
+    def mover_traz(self):
+        self.image = pygame.transform.flip(self.image,True,False)
+        self.mover = True
+        self.rect.x -= 10
+        if self.rect.x <= 10:
+            self.rect.x = 10
+
+
+    def update(self):
+     if self.mover == True:
+        self.image_atual = (self.image_atual + 1) % 4
+        self.image =  self.Sprites[self.image_atual]
+        self.image = pygame.transform.scale(self.image, (80, 80))
+     if not key[pygame.K_RIGHT]:
+         self.mover = False
+
+
+
+class BackGround(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/fundo01.png')
+        self.rect = self.image.get_rect()
+        self.rect.topleft = 0,0
+
+    def update(self):
+     if jogo.rect.x >= 520 and  key[pygame.K_RIGHT]:
+       self.rect.x -= 10
+     if jogo.rect.x >= 10 and  key[pygame.K_LEFT]:
+        self.rect.x += 10
+        #borda
+     if self.rect.x >= 0 and jogo.rect.x >= 10:
+          self.rect.x = 0
+
+     if self.rect.x <= -1700 and jogo.rect.x >= 520:
+        self.rect.x = -1700
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self):
+
+        pygame.sprite.Sprite.__init__(self)
+        self.bullets = []
+        self.image = pygame.image.load('img/bullet.png')
+        self.rect = self.image.get_rect()
+        self.rect.topleft = jogo.rect.x + 52 ,jogo.rect.y + 32
+
+    def update(self):
+      self.rect.x += 10
+
+
+FPS = 6
+pygame.init()
+
+
+
+
+
+
+Player_Groups = pygame.sprite.Group()
+jogo = Jogo()
+Player_Groups.add(jogo)
+
+Bullet_Group = pygame.sprite.Group()
+bullet = Bullet()
+Bullet_Group.add(bullet)
+
+
+Fundo_Group = pygame.sprite.Group()
+plano_fund = BackGround()
+Fundo_Group.add(plano_fund)
+
+relogio = pygame.time.Clock()
+
+while True:
+
+
+    tiro_sound = pygame.mixer.Sound('music/bullet.wav')
+    tela.fill((0,0,0))
+    relogio.tick(20)
+
+    for event in pygame.event.get():
+        if event.type == "Exit":
+            pygame.quit()
+            sys.exit()
+
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_RIGHT]:
+       jogo.mover_frente()
+
+
+
+    if key[pygame.K_LEFT]:
+       jogo.mover_traz()
+
+
+
+
+
+    if key[pygame.K_q]:
+       pygame.quit()
+
+    if key[pygame.K_SPACE ]:
+
+       Bullet_Group.add(Bullet())
+       tiro_sound.play()
+
+
+
+    Fundo_Group.draw(tela)
+    Fundo_Group.update()
+
+    Player_Groups.draw(tela)
+    Player_Groups.update()
+
+    Bullet_Group.draw(tela)
+    Bullet_Group.update()
+
+
+
+    pygame.display.update()
+
+
